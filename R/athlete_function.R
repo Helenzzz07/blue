cfec5ca0d5a6c37cf8058fec3479297eaa160258
Helenzzz07 <hzhou70@smith.edu>
2025-04-30 09:00:48 -0400

    DAY2 partially done

--- R/athlete_function.R
+++ R/athlete_function.R
@@ -1,16 +1,13 @@
 find_athlete_url <- function(athlete_name) {
 
-  file_path <- system.file("results.csv", package = "blue")
+  file_path <- system.file("olympic_results.rda", package = "blue")
 
   if (file_path == "") {
     stop("results.csv not found in the installed blue package.")
   }
 
 
-  data <- read.csv(file_path, stringsAsFactors = FALSE)
-
-
-  match_row <- data[data$name == athlete_name, ]
+  match_row <- olympic_results[olympic_results$name == athlete_name, ]
 
   if (nrow(match_row) == 0) {
     warning("Athlete not found.")
@@ -18,5 +15,3 @@ find_athlete_url <- function(athlete_name) {
   }
   return(match_row$results_url)
 }
-
-athlete_data <- data[data$name == athlete_name, ]
